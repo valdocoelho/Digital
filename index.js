7b7c34cecfe7ce7bef4ea66aa1270a2cbaf8c1c0
ffelipesimoes <ffelipesimoes@users.noreply.github.com>
2018-06-28 17:17:52 -0300

    express 1.0

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure and GIT!");
+    response.end("Hello Azure and GIT and node");
 
 });
 
