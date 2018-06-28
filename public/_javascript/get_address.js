767c9a43b95ad7a43757c0fbed91a19b53286b48
ffelipesimoes <ffelipesimoes@users.noreply.github.com>
2018-06-28 18:01:31 -0300

    cleaning

--- public/_javascript/get_address.js
+++
@@ -1,7 +0,0 @@
-var bitcore = require('bitcore-lib');
-function getwallet(wallet){
-	var wallet = document.getElementById('wallet').value;
-   	var address  = bitcore.Address;
-    //console.log(wallet, "Is valid?", address.isValid(wallet));
-    document.getElementById("wallet_result").value = address.isValid(wallet);
-	}
