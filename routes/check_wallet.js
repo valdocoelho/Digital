4252000bab88d3030ad4950fd2d0ed13c6da818c
ffelipesimoes <ffelipesimoes@users.noreply.github.com>
2018-06-28 22:34:35 -0300

    add Check_wallet page v0.4

--- routes/check_wallet.js
+++ routes/check_wallet.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/check_wallet', function(req, res, next) {
-  res.render('check_wallet', { title: 'Checking Wallet' });
+  res.render('check_wallet.html', { title: 'Checking Wallet' });
 });
 
 module.exports = router;
